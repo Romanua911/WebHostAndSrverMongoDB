b6fef3048fe21de31358fafd502fec8ff1e65e25
Romanua911 <kuc911@gmail.com>
2018-09-28 23:31:26 +0300

    FIX Test Mongo DB in console

--- WebHost/src/main/java/WebHost/Application.java
+++ WebHost/src/main/java/WebHost/Application.java
@@ -24,27 +24,27 @@ public class Application implements CommandLineRunner {
 		repository.save(new Customer("Alice", "Smith"));
 		repository.save(new Customer("Bob", "Smith"));
 		
-//		System.out.println("Customers found with findAll():");
-//		System.out.println("-------------------------------");
-//		for (Customer customer : repository.findAll()) {
-//			System.out.println(customer);
-//		}
-//		System.out.println();
-//		
-//		repository.delete(Customer())
-//
-//		System.out.println();
-//
-//		// fetch an individual customer
-//		System.out.println("Customer found with findByFirstName('Alice'):");
-//		System.out.println("--------------------------------");
-//		System.out.println(repository.findByFirstName("Alice"));
-//
-//		System.out.println("Customers found with findByLastName('Smith'):");
-//		System.out.println("--------------------------------");
-//		for (Customer customer : repository.findByLastName("Smith")) {
-//			System.out.println(customer);
-//		}
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+		
+		repository.delete(Customer())
+
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
 		
 	}
 
