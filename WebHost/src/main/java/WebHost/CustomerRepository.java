e4b09c835de2cf90d91e1259619cdaed0ca67e32
Romanua911 <kuc911@gmail.com>
2018-10-02 00:19:54 +0300

    CRUD repository

--- WebHost/src/main/java/WebHost/CustomerRepository.java
+++ WebHost/src/main/java/WebHost/CustomerRepository.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
+	
 	public Customer findByFirstName(String firstName);
+	//one
 	public List<Customer> findByLastName(String lastName);
+	//many
 }
+
